27d96f27cfe6af0bc8d0f4263ab3360f3771c3c7
Xtremehonor <Xtremehonor@gmail.com>
2024-05-12 01:48:24 -0700

    add 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
