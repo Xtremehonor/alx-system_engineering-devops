af7d8bb9052260f8f49a2a9e890d505cb9620f09
Xtremehonor <Xtremehonor@gmail.com>
2024-05-12 02:46:03 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #!/usr/bin/pup
+
+#Install Werkzeug (2.1.1)
+package {'Werkzeug':
+  ensure =>  '2.1.1',
+  provider => 'pip3'
+  }
+
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
